002adec695cffb3b609118fe3718818e51201541
Wensie <wcaillet@MacBook-Pro.local>
2016-02-03 15:26:57 -0600

    adding homework1

---
+++ myProject/TIYHomework/shelby.sh
@@ -0,0 +1,10 @@
+mkdir shelby
+cd ./shelby
+touch README fizz.txt buzz.txt deleteme.txt
+echo fizzbuzz > README
+mkdir texts ##Creating a new directory doesn't automatically move you into it
+
+mv *.txt ./texts
+rm deleteme.txt
+
+# comment
